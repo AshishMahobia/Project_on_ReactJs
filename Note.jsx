5198fdea1a7f15ce42e22727139dd4e2fe085953
Ashish Kumar Mahobia <63652300+AshishMahobia@users.noreply.github.com>
2021-06-05 17:43:07 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Ashish Kumar Mahobia </h1>
+      <p>"Know the difference between enjoying your Youth and destroying your Future." </p>
+      </div>
+  );
+}
+
+export default Note;
